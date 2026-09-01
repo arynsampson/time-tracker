82bbb732a65bb683684f4145b56398f16dceab01
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2026-09-01 21:01:01 +0200

    Update Projects component

--- src/views/Projects.tsx
+++ src/views/Projects.tsx
@@ -1,16 +1,80 @@
+import { useEffect, useState } from "react";
+
 import Button from "../components/Button";
+import Modal from "../components/Modal";
 import ProjectItem from "../components/ProjectItem";
 
+type Project = {
+  id: string;
+  projectName: string;
+  projectDescription: string;
+};
+
 export default function Projects() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [projectName, setProjectName] = useState("");
+  const [projectDescription, setProjectDescription] = useState("");
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  useEffect(() => {
+    localStorage.setItem("projects", JSON.stringify(projects));
+  }, [projects]);
+
+  const handleAddProject = () => {
+    setProjects([
+      ...projects,
+      {
+        id: crypto.randomUUID(),
+        projectName,
+        projectDescription,
+      },
+    ]);
+
+    setProjectName("");
+    setProjectDescription("");
+    setIsOpen(false);
+  };
+
   return (
     <>
-      <div className="projects">
+      <div className="projects view">
         <h2>Projects</h2>
-        <Button text="New Project" />
+        <Button text="New Project" setIsOpen={setIsOpen} />
         <div className="projects-list">
-          <ProjectItem />
-          <ProjectItem />
+          {projects.map((project) => {
+            return (
+              <ProjectItem
+                key={project.id}
+                projectName={project.projectName}
+                projectDescription={project.projectDescription}
+              />
+            );
+          })}
         </div>
+        {isOpen && (
+          <Modal setIsOpen={setIsOpen}>
+            <h3>Add new Project</h3>
+            <div className="form-container">
+              <label htmlFor="proeject-name">Enter Project name:</label>
+              <input
+                type="text"
+                name="project-name"
+                value={projectName}
+                onChange={(e) => setProjectName(e.target.value)}
+                required
+              />
+              <textarea
+                name="project-description"
+                id="project-description"
+                value={projectDescription}
+                onChange={(e) => setProjectDescription(e.target.value)}
+              />
+              <button type="submit" onClick={() => handleAddProject()} disabled={projectName.length < 3}>
+                Add project
+              </button>
+            </div>
+          </Modal>
+        )}
       </div>
     </>
   );
