9e3750135ad50c3797a81b1bd79289b307518774
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2026-09-01 20:59:39 +0200

    Update Button component
    - added prop types
    - added onclick event to open modal

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,12 @@
-export default function Button({ text }) {
-  return <button className="btn">+ {text}</button>;
+type ButtonProps = {
+  text: string;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export default function Button({ text, setIsOpen }: ButtonProps) {
+  return (
+    <button className="btn" onClick={() => setIsOpen(true)}>
+      + {text}
+    </button>
+  );
 }
