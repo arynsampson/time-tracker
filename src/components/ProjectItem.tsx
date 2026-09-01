03b3c1fd0895955462ea607663af8d699da69274
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2026-09-01 21:00:11 +0200

    Update ProjectItem component with data & prop types

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -1,15 +1,20 @@
 import { EllipsisVertical } from "lucide-react";
 
-export default function ProjectItem() {
+type ProjectItemProps = {
+  projectName: string;
+  projectDescription: string;
+};
+
+export default function ProjectItem({ projectName, projectDescription }: ProjectItemProps) {
   return (
     <div className="project-item">
       <div className="project-colour"></div>
       <div className="project-info">
-        <p className="project-name">Test project</p>
-        <p className="project-description">This is my first project</p>
+        <p className="project-name">{projectName}</p>
+        <p className="project-description">{projectDescription}</p>
       </div>
       <p className="project-hours">
-        12.5 <span>hrs</span>
+        0 <span>hrs</span>
       </p>
       <div className="context-menu">
         <EllipsisVertical size="14px" />
