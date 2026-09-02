f1487c3f9ea11cdd4c60265a76ae2c995cfef5a6
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2026-09-02 17:02:44 +0200

    Update modal component styles & structure

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,13 +1,52 @@
+import { X } from "lucide-react";
+
 type ModalProps = {
   children: React.ReactNode;
+  heading: string;
+  submitDisabled: boolean;
+  resetStateData: () => void;
+  handleAddProject: () => void;
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-export default function Modal({ children, setIsOpen }: ModalProps) {
+export default function Modal({
+  children,
+  heading,
+  submitDisabled,
+  resetStateData,
+  handleAddProject,
+  setIsOpen,
+}: ModalProps) {
+  const handleCloseModal = () => {
+    setIsOpen(false);
+    resetStateData();
+  };
+
   return (
     <div className="backdrop">
-      <button onClick={() => setIsOpen(false)}>Close modal</button>
-      <div className="modal">{children}</div>
+      <div className="modal">
+        <div className="modal-heading-section">
+          <h3>{heading}</h3>
+          <button onClick={() => handleCloseModal()} className="close-modal">
+            <X color="#6B7078" size="18px" />
+          </button>
+        </div>
+        {children}
+
+        <div className="modal-footer">
+          <button className="btn btn-secondary" onClick={() => handleCloseModal()}>
+            Cancel
+          </button>
+          <button
+            className="btn btn-primary"
+            type="submit"
+            onClick={() => handleAddProject()}
+            disabled={submitDisabled}
+          >
+            Create Project
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
