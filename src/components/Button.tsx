c2cc21f5a253da847c67c8132c91e96fbb9ab0a3
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2026-09-15 20:25:28 +0200

    Update Button component to use other modals
    This is a temporary solution to get the functionality to work. A refactor will come later cleaning up the components and making them more dynamic

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,17 @@
 type ButtonProps = {
   text: string;
-  setAddProjectModalisOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setAddTaskModalisOpen?: React.Dispatch<React.SetStateAction<boolean>>;
+  setAddProjectModalisOpen?: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-export default function Button({ text, setAddProjectModalisOpen }: ButtonProps) {
+export default function Button({ text, setAddTaskModalisOpen, setAddProjectModalisOpen }: ButtonProps) {
+  const handleClick = () => {
+    if (text === "New Project") setAddProjectModalisOpen?.(true);
+    if (text === "New Task") setAddTaskModalisOpen?.(true);
+  };
+
   return (
-    <button className="btn btn-primary" onClick={() => setAddProjectModalisOpen(true)}>
+    <button className="btn btn-primary" onClick={() => handleClick()}>
       + {text}
     </button>
   );
