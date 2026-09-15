698b2dd83aefb83be8f00ced58a40032def0cb10
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2026-09-15 20:27:43 +0200

    Add initial Tasks view
    This is a temporary solution to get the functionality to work. A refactor will come later cleaning up the components

--- src/views/Tasks.tsx
+++ src/views/Tasks.tsx
@@ -1,7 +1,44 @@
+import { useState } from "react";
+
+import Button from "../components/Button";
+import NewTaskModal from "../components/NewTaskModal";
+
+export type Task = {
+  name: string;
+  startTime: string;
+  endTime: string;
+  date: string;
+};
+
 export default function Tasks() {
+  const [addTaskModalisOpen, setAddTaskModalisOpen] = useState(false);
+  const [taskTitle, setTaskTitle] = useState("");
+
+  const resetStateData = () => {
+    setTaskTitle("");
+  };
+
+  const handleAddNewTask = () => {};
+
   return (
     <>
-      <div className="tasks">This is the tasks view</div>
+      <div className="tasks view-with-btn">
+        <h2>Tasks</h2>
+        <Button text="New Task" setAddTaskModalisOpen={setAddTaskModalisOpen} />
+        {addTaskModalisOpen && (
+          <NewTaskModal
+            heading="Create New Task"
+            submitButtonCopy="Create Task"
+            taskTitle={taskTitle}
+            submitDisabled={taskTitle.length < 3}
+            resetStateData={resetStateData}
+            handleSubmitData={handleAddNewTask}
+            setTaskTitle={setTaskTitle}
+            setAddTaskModalisOpen={setAddTaskModalisOpen}
+          />
+        )}
+        <div className="tasks-content-display">Tasks</div>
+      </div>
     </>
   );
 }
