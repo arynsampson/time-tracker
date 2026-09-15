8ed541233b5088e1350113e61de9d12a1c773844
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2026-09-15 13:59:55 +0200

    Initial TimeLog styling

--- src/views/TimeLog.tsx
+++ src/views/TimeLog.tsx
@@ -1,7 +1,37 @@
+import { Trash } from "lucide-react";
+
 export default function TimeLog() {
   return (
     <>
-      <div className="time-log">This is the time log view</div>
+      <div className="time-log view">
+        <h2>Time Log</h2>
+        <div className="table-container">
+          <table>
+            <thead>
+              <tr>
+                <th>Task</th>
+                <th>Project</th>
+                <th>Start Time</th>
+                <th>End Time</th>
+                <th>Duration</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Design Landing Page</td>
+                <td>Website Redesign</td>
+                <td>Today, 10:00 AM</td>
+                <td>Today, 11:30 AM</td>
+                <td>1.5 hours</td>
+                <td>
+                  <Trash color="#000" />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
   );
 }
